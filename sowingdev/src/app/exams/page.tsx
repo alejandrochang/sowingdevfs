2f5048ca1e74d0821769d6f97e5e9781647c258c
Alejandro Chang <alejandrochang10@gmail.com>
2023-12-03 21:43:08 -0800

    feat(sd): setting up basic routing pages

---
+++ sowingdev/src/app/exams/page.tsx
@@ -0,0 +1,3 @@
+export default function Exams() {
+  return <div>Exams</div>
+}
--- sowingdev/src/app/page.tsx
+++ sowingdev/src/app/page.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
+// Homepage
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
