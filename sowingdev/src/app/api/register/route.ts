895f73cb622e0e441615ed25aee17dba6782d243
Alejandro Chang <alejandrochang10@gmail.com>
2023-12-13 22:11:07 -0800

    feat(sd): adding bcrypt and hashed pass logic

--- sowingdev/src/app/api/register/route.ts
+++ sowingdev/src/app/api/register/route.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
-export async function POST(req: any) {
+export async function POST(req: Request) {
   try {
     const { email, password, name } = await req.json();
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    console.log('plop success:', { email, password, name });
+    console.log('plop success:', { email, password, hashedPassword, name });
     return NextResponse.json({ message: "User registered" }, { status: 201 });
   } catch (error) {
     return NextResponse.json({ message: "An error occured while registering user." } , { status: 500 });
