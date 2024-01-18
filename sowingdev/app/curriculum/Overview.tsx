d9942e1ea83ad53eacc1aa0b4eae2d0b06d09156
Alejandro Chang <alejandrochang10@gmail.com>
2024-01-17 17:17:48 -0800

    feat(sd): content update overview

--- sowingdev/app/curriculum/Overview.tsx
+++ sowingdev/app/curriculum/Overview.tsx
@@ -8,18 +8,21 @@ interface OverviewProps {
 }
 
 const Overview: React.FC<OverviewProps> = (props) => {
-  const itemsData = listItems.slice(0, 7);
+  const itemsData = listItems.slice(0, 6);
 
   return (
     <div>
       <Typography variant='body2' gutterBottom>
         Welcome to the SowingDev Mentorship Program! We&apos;re delighted to
-        have you embark on this journey of growth and knowledge with us. We are
-        delighted to partner with you in your coding journey our community is
-        dedicated to nurturing talent, fostering collaboration, and helping you
-        thrive in the world of development. Together, we&apos;ll cultivate
+        have you embark on this journey of growth and programming knowledge with us. 
+        In this Overview Section of the curriculum you can find a lot of examples and resources 
+        that can be beneficial for your learning journey. These resources include Frontend, Backend, Web Technologies and interview prep material.
+        There is no hard requirement to go through any of these resources, but we highly recommend you to do so. Additionally, with the 
+        mentorship you will have a more curated plan of what to do and what to learn. So if you 
+        feel overwhelmed or dont know where to start, don&apos;t worry, your mentor will help you.
+        Together, we&apos;ll cultivate
         skills, reap the rewards of mentorship, and sow the seeds of excellence.
-        Let&apos;s sow, grow, and succeed together!
+        Let&apos;s sow, grow, and succeed together! I hope youre as excited as we are!
       </Typography>
       <br />
       <Typography variant='body2' gutterBottom>
@@ -40,12 +43,12 @@ const Overview: React.FC<OverviewProps> = (props) => {
       <div>
         {itemsData.map(({ title, content }, idx) => {
           return (
-            <>
-              <Typography variant='body2' key={title} gutterBottom>
+            <div key={title}>
+              <Typography variant='body2' gutterBottom>
                 {title}
               </Typography>
               <Typography variant='body2' paragraph>{content}</Typography>
-            </>
+            </div>
           );
         })}
       </div>
